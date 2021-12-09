b85182b20d2cb34bee56dbbb8b20ad3cb0aba339
Hz-design <70894669+Hz-design@users.noreply.github.com>
2021-12-09 14:08:35 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
 // JavaScript Document
+// Navigatie hamburger menu open en dicht
+var deButton = document.querySelector(".hamburger-menu");
+console.log(deButton)
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  deNav = document.querySelector("header nav span:nth-of-type(2)");
+  deNav.classList.toggle("toonMenu");
+}
+
+//Making a background image scroll slower than everything else
+document.querySelector("body").onscroll = function myFunction(){
+  var scrolltotop = document.scrollingElement.scrollTop;
+  var target = document.querySelector(".homepage section:first-of-type");
+  var xvalue = "center";
+  var factor = 0.5;
+  var yvalue = scrolltotop * factor;
+  target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+}
